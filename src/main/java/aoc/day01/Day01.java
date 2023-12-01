9b1f2e6f99023ebbabcaacd555cad88fc12fea78
Marta Basto <martabasto2@gmail.com>
2023-12-01 17:44:06 +0000

    add day 1 part I

--- src/main/java/aoc/day01/Day01.java
+++ src/main/java/aoc/day01/Day01.java
@@ -8,12 +8,61 @@ public class Day01 implements Day {
 
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : input.get(0);
+        if ( input == null  || input.isEmpty() )
+        {
+            throw new IllegalArgumentException( "Input is empty" );
+        }
+
+        int sum = 0;
+        for ( String line : input ){
+            sum += getValues( line );
+        }
+
+        return String.valueOf( sum );
     }
 
     @Override
     public String part2(List<String> input) {
         return input.isEmpty() ? "" : input.get(0);
+    }
+
+    private int getValues(String line) {
+        if ( line == null  || line.isEmpty())
+        {
+            throw new IllegalArgumentException( "Line is empty" );
+        }
+
+        // Find location of digits
+        int firstDigitIndex = findFirstDigitIndex(line);
+        int lastDigitIndex = findLastDigitIndex(line);
+
+        // Extract digits
+        char firstDigit = line.charAt( firstDigitIndex );
+        char lastDigit = line.charAt( lastDigitIndex );
+
+        // Convert to int and combine them
+        return Integer.parseInt( "" + firstDigit + lastDigit );
+
+    }
+
+    private int findFirstDigitIndex(String line){
+        for ( int i = 0; i < line.length(); i++ )
+        {
+            if ( Character.isDigit( line.charAt( i ) ) ) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("Line does not contain any digits");
+    }
+
+    private int findLastDigitIndex(String line){
+        for ( int i = line.length() - 1; i >= 0; i-- )
+        {
+            if ( Character.isDigit( line.charAt( i ) ) ){
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("Line does not contain any digits");
     }
 
 }
--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -6,19 +6,57 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class Day01Test {
 
     @Test
-    public void testPart1(){
+    public void givenInputLines_whenCalculatingCalibrationValue_thenCorrectValuesReturned(){
         // Given
-        List<String> input = Collections.singletonList("test");
+        List<String> inputLines = List.of( "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" );
 
         // When
-        String result = new Day01().part1(input);
+        String actualResult = new Day01().part1(inputLines);
 
         // Then
-        assertEquals(input.get(0), result);
+        assertEquals("142", actualResult);
+    }
+
+    @Test
+    public void givenEmptyInputLines_whenCalculatingCalibrationValue_thenThrowIllegalArgumentException(){
+        // Given
+        List<String> emptyLines = List.of( "", "", "", "" );
+
+        // When
+        IllegalArgumentException actualResult = assertThrows(IllegalArgumentException.class, () -> new Day01().part1(emptyLines));
+
+        // Then
+        assertEquals( "Line is empty", actualResult.getMessage() );
+    }
+
+    @Test
+    public void givenInputLinesWithoutDigits_whenCalculatingCalibrationValue_thenThrowIllegalArgumentException(){
+        // Given
+        List<String> linesWithoutDigits = List.of( "sdfghngfs", "asggndf", "creakeg", "pogthbs" );
+
+        // When
+        IllegalArgumentException actualResult = assertThrows(IllegalArgumentException.class, () -> new Day01().part1(linesWithoutDigits));
+
+        // Then
+        assertEquals( "Line does not contain any digits", actualResult.getMessage() );
+    }
+
+    @Test
+    public void givenEmptyInput_whenCalculatingCalibrationValue_thenThrowIllegalArgumentException(){
+
+        //Given
+        List<String> emptyInput = Collections.emptyList();
+
+        // When
+        IllegalArgumentException actualResult = assertThrows(IllegalArgumentException.class, () -> new Day01().part1(emptyInput));
+
+        // Then
+        assertEquals( "Input is empty", actualResult.getMessage() );
     }
 
     @Test
@@ -32,4 +70,6 @@ public class Day01Test {
         // Then
         assertEquals(input.get(0), result);
     }
+
+
 }
