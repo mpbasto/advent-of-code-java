2c5afcdbc7f02dee28f972c2a3f095010f5c48e4
Marta Basto <martabasto2@gmail.com>
2023-12-03 12:00:20 +0000

    add day 2

--- src/main/java/aoc/day02/Day02.java
+++ src/main/java/aoc/day02/Day02.java
@@ -2,20 +2,166 @@ package aoc.day02;
 
 import aoc.Day;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day02
     implements Day
 {
+    public static final int RED = 12;
+    public static final int BLUE = 14;
+    public static final int GREEN = 13;
+
     @Override
     public String part1( final List<String> input )
     {
-        return input.isEmpty( ) ? "" : input.get( 0 );
+        if ( input == null || input.isEmpty( ) )
+        {
+            throw new IllegalArgumentException( "Input is empty" );
+        }
+
+        List<Integer> possibleGames = new ArrayList<>( );
+        boolean isPossibleGame;
+
+        for ( String line : input )
+        {
+            int gameId = getGameId( line );
+            String[] sets = extractSetsArray( line );
+            isPossibleGame = true;
+
+            for ( String set : sets )
+            {
+                // Extract number of cubes of each color
+                Integer redTotal = countCubes( set,
+                                               "red" );
+                Integer blueTotal = countCubes( set,
+                                                "blue" );
+                Integer greenTotal = countCubes( set,
+                                                 "green" );
+
+                if ( !( blueTotal <= BLUE && greenTotal <= GREEN && redTotal <= RED ) )
+                {
+                    isPossibleGame = false;
+                    break;
+                }
+            }
+
+            if ( isPossibleGame )
+            {
+                possibleGames.add( gameId );
+            }
+        }
+
+        int sum = 0;
+        for ( int gameId : possibleGames )
+        {
+            sum += gameId;
+        }
+
+        return String.valueOf( sum );
+    }
+
+    private static int getGameId( String line )
+    {
+
+        // Define regex pattern and matcher
+        Pattern pattern = Pattern.compile( "Game (\\d+):" );
+        Matcher matcher = pattern.matcher( line );
+
+        // Check if pattern found
+        if ( matcher.find( ) )
+        {
+            // Get gameId
+            return Integer.parseInt( matcher.group( 1 ) );
+
+        }
+
+            throw new IllegalArgumentException( "Invalid line. Could not find pattern" );
+    }
+
+    private static String[] extractSetsArray( String line )
+    {
+        // Define regex pattern and matcher
+        Pattern pattern = Pattern.compile( ": (.*)" );
+        Matcher matcher = pattern.matcher( line );
+
+        // Check if pattern found
+        if ( matcher.find( ) )
+        {
+            // Extract set
+            String set = matcher.group( 1 );
+
+            // Split sets
+            return set.split( ";" );
+        }
+
+        throw new IllegalArgumentException( "Invalid line. Could not find pattern" );
+    }
+
+    private static Integer countCubes( final String set,
+                                       final String colour )
+    {
+        // Define pattern and matcher
+        Pattern colourPattern = Pattern.compile( "\\b(\\d+) " + colour + "\\b" );
+        Matcher matcher = colourPattern.matcher( set );
+
+        // Check for pattern
+        if ( matcher.find( ) )
+        {
+            // Convert to int
+            return Integer.parseInt( matcher.group( 1 ) );
+        }
+
+        return 0;
     }
 
     @Override
     public String part2( final List<String> input )
     {
-        return input.isEmpty( ) ? "" : input.get( 0 );
+        if ( input == null || input.isEmpty( ) )
+        {
+            throw new IllegalArgumentException( "Input is empty" );
+        }
+
+        List<Integer> red = new ArrayList<>( );
+        List<Integer> blue = new ArrayList<>( );
+        List<Integer> green = new ArrayList<>( );
+        List<Integer> games = new ArrayList<>( );
+        int powers;
+
+        for ( String line : input )
+        {
+            String[] sets = extractSetsArray( line );
+
+            for ( String set : sets )
+            {
+                red.add( countCubes( set,
+                                     "red" ) );
+                blue.add( countCubes( set,
+                                      "blue" ) );
+                green.add( countCubes( set,
+                                       "green" ) );
+            }
+
+            powers = Collections.max( red ) * Collections.max( blue ) * Collections.max( green );
+            games.add( powers );
+
+            // Reset counters
+            red.clear( );
+            blue.clear( );
+            green.clear( );
+        }
+
+        // Sum all the powers
+        int sum = 0;
+        for ( int game : games )
+        {
+            sum += game;
+        }
+
+        return String.valueOf( sum );
     }
 }
--- src/test/java/aoc/day02/Day02Test.java
+++ src/test/java/aoc/day02/Day02Test.java
@@ -6,20 +6,112 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class Day02Test
 {
     @Test
-    public void testPart1( )
+    public void givenInput_whenDeterminingPossibleGames_thenCorrectGameIdSumReturned( )
     {
         // Given
-        List<String> input = Collections.singletonList( "test" );
+        List<String> input = List.of( "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                                                             "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                                                             "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                                                             "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                                                             "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" );
 
         // When
         String result = new Day02( ).part1( input );
 
         // Then
-        assertEquals( input.get( 0 ),
+        assertEquals( "8",
                       result );
+    }
+
+    @Test
+    public void givenEmptyInput_whenDeterminingPossibleGames_thenThrowIllegalArgumentException( )
+    {
+
+        //Given
+        List<String> emptyInput = Collections.emptyList( );
+
+        // When
+        IllegalArgumentException actualResult = assertThrows( IllegalArgumentException.class,
+                                                              ( ) -> new Day02( ).part1( emptyInput ) );
+
+        // Then
+        assertEquals( "Input is empty",
+                      actualResult.getMessage( ) );
+    }
+
+
+    @Test
+    public void givenInput_whenFindingMinimumSet_thenCorrectSetPowerSumReturned( )
+    {
+        // Given
+        List<String> input = List.of( "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                                      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                                      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                                      "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                                      "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" );
+
+        // When
+        String result = new Day02( ).part2( input );
+
+        // Then
+        assertEquals( "2286",
+                      result );
+    }
+
+    @Test
+    public void givenEmptyInput_whenFindingMinimumSet_thenThrowIllegalArgumentException( )
+    {
+
+        //Given
+        List<String> emptyInput = Collections.emptyList( );
+
+        // When
+        IllegalArgumentException actualResult = assertThrows( IllegalArgumentException.class,
+                                                              ( ) -> new Day02( ).part2( emptyInput ) );
+
+        // Then
+        assertEquals( "Input is empty",
+                      actualResult.getMessage( ) );
+    }
+
+    @Test
+    public void givenInputWithWrongGameIdPattern_whenFindingMinimumSet_thenThrowIllegalArgumentException( )
+    {
+
+        //Given
+        List<String> input = List.of( "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                                      "Game 2 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                                      "Fame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red" );
+
+        // When
+        IllegalArgumentException actualResult = assertThrows( IllegalArgumentException.class,
+                                                              ( ) -> new Day02( ).part2( input ) );
+
+        // Then
+        assertEquals( "Invalid line. Could not find pattern",
+                      actualResult.getMessage( ) );
+    }
+
+    @Test
+    public void givenInputWithWrongPattern_whenFindingMinimumSet_thenThrowIllegalArgumentException( )
+    {
+
+        //Given
+        List<String> input = List.of( "Game 1 - 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                                      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                                      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red" );
+
+        // When
+        IllegalArgumentException actualResult = assertThrows( IllegalArgumentException.class,
+                                                              ( ) -> new Day02( ).part2( input ) );
+
+        // Then
+        assertEquals( "Invalid line. Could not find pattern",
+                      actualResult.getMessage( ) );
     }
 }
